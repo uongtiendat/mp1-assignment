75c96c4c28e98dd715985b3635ee281b4aefd423
12010051.UongTienDat <dat.hktg@gmail.com>
2015-10-29 11:00:42 +0700

    update

--- MP1Node.cpp
+++ MP1Node.cpp
@@ -284,7 +284,22 @@ void MP1Node::onHeartbeat(Address* addr, void* data, size_t size) {
 }
 
 void MP1Node::UpdateMemberList(Address *addr, long heartbeat)  {
-	
+	vector<MemberListEntry>::iterator it;
+	for (it = memberNode->memberList.begin(); it != memberNode->memberList.end(); it++) {
+		if ((AddressFromMLE(&(*it)) == *addr) == 0) {
+				if (heartbeat > it->getheartbeat()) {
+					it->setheartbeat(heartbeat);
+					it->settimestamp(par>getcurrtime());
+						return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		MemberListEntry mle(*((int*)addr->addr),*((short*)&(addr->addr[4])),heartbeat,par->getcurrtime());
+		memberNode->memberList.push_back(mle);
+		log->logNodeAdd(&memberNode->addr, addr);
+		return true;
 }
 
 /**
@@ -328,13 +343,6 @@ Address MP1Node::getJoinAddress() {
 
     return joinaddr;
 }
-
-//MemberListEntry MLEFromAddr(Address* addr) {
-//	MemberListEntry mle(0, 0);
-//	memcpy(&mle.id, addr->addr, sizeof(int));
-//	memcpy(&mle.port, addr->addr + 4, sizeof(short));
-//	return mle;
-//}
 
 /**
  * FUNCTION NAME: initMemberListTable
